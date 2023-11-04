18b23cc3fd1f7718c4778d391563db674b7b89cd
Sheila <101499671+shgonzals@users.noreply.github.com>
2023-11-04 12:34:58 +0100

    📚 learning mongodb from scratch

---
+++ src/main/java/com/shgonzal/mongodbgettingstarted/MongodbGettingStartedApplication.java
@@ -0,0 +1,13 @@
+package com.shgonzal.mongodbgettingstarted;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongodbGettingStartedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongodbGettingStartedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shgonzal/mongodbgettingstarted/MongodbGettingStartedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shgonzal.mongodbgettingstarted;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongodbGettingStartedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
